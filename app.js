97a5872a3f8761439a1a0f9c0b2657955d5554da
Adrian Marten <Adrian.Marten@msg.group>
2020-06-26 07:43:56 +0200

    Removed the cli- file because it was unnecessary

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
 
-const cli = require('./cli');
-cli.run(process);
+const program = require('commander');
+const commands = require('./commands');
+const packageInfo = require('./package.json');
+
+const run = oProcess => {
+    program.version(packageInfo.version);
+
+    Object.keys(commands).forEach(key => commands[key].createCommand(program));
+    program.parse(oProcess.argv);
+}
+
+run(process);
